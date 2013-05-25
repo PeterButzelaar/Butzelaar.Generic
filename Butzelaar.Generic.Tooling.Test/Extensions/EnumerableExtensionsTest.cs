57b1aee4cf2b8e7330fd7552861976ba542e1b94
Nemesish3d <peter.butzelaar@gmail.com>
2013-05-25 17:48:50 +0200

    Tooling project added

---
+++ Butzelaar.Generic.Tooling.Test/Extensions/EnumerableExtensionsTest.cs
@@ -0,0 +1,49 @@
+﻿using Butzelaar.Generic.Tooling.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Butzelaar.Generic.Tooling.Test.Extensions
+{
+    [TestClass]
+    public class EnumerableExtensionsTest
+    {
+        [TestMethod]
+        public void ForEach_StringBuilderAppendEmpty()
+        {
+            var sb = new StringBuilder();
+            IEnumerable<string> list = new List<string>();
+            list.ForEach(s => sb.Append(s));
+            Assert.AreEqual(sb.ToString(), string.Empty);
+        }
+
+        [TestMethod]
+        public void ForEach_StringBuilderAppendNull()
+        {
+            var sb = new StringBuilder();
+            IEnumerable<string> list = new List<string> { null };
+            list.ForEach(s => sb.Append(s));
+            Assert.AreEqual(sb.ToString(), string.Empty);
+        }
+
+        [TestMethod]
+        public void ForEach_StringBuilderAppendOneTitle()
+        {
+            const string title = "ForEach";
+            var sb = new StringBuilder();
+            IEnumerable<string> list = new List<string> { title };
+            list.ForEach(s => sb.Append(s));
+            Assert.AreEqual(sb.ToString(), title);
+        }
+
+        [TestMethod]
+        public void ForEach_StringBuilderAppendMoreTitles()
+        {
+            const string title = "ForEach";
+            var sb = new StringBuilder();
+            IEnumerable<string> list = new List<string> { title, title };
+            list.ForEach(s => sb.Append(s));
+            Assert.AreEqual(sb.ToString(), title + title);
+        }
+    }
+}
---
+++ Butzelaar.Generic.Tooling/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Butzelaar.Generic.Tooling.Extensions
+{
+    /// <summary>
+    /// Contains extension methods
+    /// </summary>
+    public static class EnumerableExtensions
+    {
+        /// <summary>
+        /// Executes the action on each item in the list
+        /// </summary>
+        /// <typeparam name="T">Type that the list contains</typeparam>
+        /// <param name="list">The list.</param>
+        /// <param name="action">The action.</param>
+        public static void ForEach<T>(this IEnumerable<T> list, Action<T> action)
+        {
+            foreach (var item in list)
+                action(item);
+        }
+    }
+}
